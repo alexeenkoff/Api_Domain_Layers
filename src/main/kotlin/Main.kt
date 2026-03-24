5362b7fc5cce6a4d2592bcc5359b24606a545acf
alexeenkoff <alexeenkoff@gmail.com>
2026-03-24 20:54:22 +0300

    feat: update Main.kt to demonstrate full data flow

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+package org.example
+
+import org.example.domain.usecase.StoresUsecase
+
+fun main() {
+    val usecase = StoresUsecase()
+    val stores = usecase.getStores()
+
+    println("=== Магазины ===")
+    println()
+    stores.forEach { store ->
+        println("ID: ${store.id.value}")
+        println("Название: ${store.name}")
+        println("Тип: ${store.type}")
+        println("Адрес: ${store.address}")
+        println("ТС: ${store.ts}")
+        println()
+    }
+
+    println("Всего магазинов: ${stores.size}")
+}
