9cfcaeb623ac9d6ba26642c2c428c07bf159dd5d
alexeenkoff <alexeenkoff@gmail.com>
2026-03-24 20:39:38 +0300

    feat: add domain value objects (StoreType, TSType, StringId)

---
+++ src/main/kotlin/org/example/domain/model/StoreType.kt
@@ -0,0 +1,7 @@
+package org.example.domain.model
+
+enum class StoreType {
+    TSX,
+    TS5,
+    TSH,
+}
---
+++ src/main/kotlin/org/example/domain/model/StringId.kt
@@ -0,0 +1,6 @@
+package org.example.domain.model
+
+@JvmInline
+value class StringId(
+    val value: String,
+)
